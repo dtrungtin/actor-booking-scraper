48bbd35be1fdccb148a4085ef496b4cf519afa05
Bui Tuan <tuanbui7101999@gmail.com>
2023-04-16 19:55:59 +0700

    fix: get hotel id

--- src/extraction/detail-page-extraction.js
+++ src/extraction/detail-page-extraction.js
@@ -72,9 +72,9 @@ const extractName = async (page) => {
 };
 
 const extractHotelId = async (page) => {
-    const btnHeart = await page.$("#wl--wl_entrypoint_hp_head [data-hotel-id]");
+    const btnHeart = await page.$("#wl--wl_entrypoint_hp_head");
     const hotelId = btnHeart
-        ? await getAttribute(btnHeart, "data-hotel-id")
+        ? await (await btnHeart.getProperty("data-hotel-id")).jsonValue()
         : null;
 
     return hotelId;
